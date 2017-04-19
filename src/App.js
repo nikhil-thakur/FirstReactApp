146aa3de74fff0cf32dcb4f83666c7f608692596
Nik Thakur <nik.thakur@daugherty.com>
2017-04-19 13:18:44 -0500

    Lesson4: Added default Props and Props to be used inside the div. Setting up and stuff

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 class App extends  React.Component {
     render(){
+        let txt = this.props.txt
         return (
             <div>
-                <h1 cassName="">Hello World</h1>
+                <h1 className="">{txt}</h1>
                 <b>Bold</b>
             </div>
         )
     }
 }
 
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "This is the default text"
+}
 
 /*const App = () => <h1> Hello World</h1>*/
 export default App
